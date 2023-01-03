29c55e4082eae805b2986c61f201cdefc780c83e
Simon Wülker <simon.wuelker@arcor.de>
2023-01-03 09:58:51 +0100

    brotli: add brotli unit tests from https://github.com/google/brotli/tree/master/tests/testdata

--- download.sh
+++ download.sh
@@ -10,9 +10,26 @@ if [ ! -f "$download_dir/fonts/roboto/Roboto-Medium.ttf" ]; then
     rm $download_dir/fonts/roboto.zip
 fi
 
-
 # Download the pre-defined brotli dictionary
 mkdir -p $download_dir/brotli
 if [ ! -f "$download_dir/brotli/dictionary" ]; then
     wget -nv -O $download_dir/brotli/dictionary https://gist.githubusercontent.com/duskwuff/8a75e1b5e5a06d768336c8c7c370f0f3/raw/0a469443ceca5c1e8a2ffed48c6ecc1570750b05/dictionary.bin
+fi
+
+# Download brotli test files
+mkdir -p $download_dir/brotli/testdata
+if [ ! -f "$download_dir/brotli/testdata/tests" ]; then
+    # # do a sparse checkout, we don't need the entire repository
+    # git init -q "$download_dir/brotli/testdata/"
+    
+    # git -C "$download_dir/brotli/testdata/" remote add origin https://github.com/google/brotli
+    # git clone --no-checkout --filter=blob:none "file://*.compressed" 
+    git clone \
+        -q \
+        --filter=blob:none  \
+        --sparse \
+        https://github.com/google/brotli \
+        "$download_dir/brotli/testdata"
+
+    git -C "$download_dir/brotli/testdata" sparse-checkout set tests/testdata
 fi
---
+++ web/http/tests/brotli.rs
@@ -0,0 +1,29 @@
+use std::{fs, io::Read};
+use http::compress::brotli;
+
+const TESTS_DIR: &'static str = "../../downloads/brotli/testdata/tests/testdata";
+
+#[test]
+fn test_brotli_decode() -> Result<(), std::io::Error> {
+    for testfile_or_error in fs::read_dir(TESTS_DIR).expect("Test files not found, did you run download.sh?") {
+        let testfile = testfile_or_error?;
+
+        if let Some(extension) = testfile.path().extension() {
+            if extension == "compressed" {
+                println!("Testing decompression of {}", testfile.path().display());
+
+                let mut compressed_buffer = vec![];
+                fs::File::open(testfile.path())?.read_to_end(&mut compressed_buffer)?;
+
+                let mut uncompressed_buffer = vec![];
+                fs::File::open(testfile.path())?.read_to_end(&mut uncompressed_buffer)?;
+
+                let decompressed = brotli::decode(&compressed_buffer).expect("Brotli decompression failed");
+
+                assert!(decompressed.iter().zip(&uncompressed_buffer).all(|(a, b)| a == b) && decompressed.len() == uncompressed_buffer.len());
+            }
+        }
+    }
+    
+    Ok(())
+}
