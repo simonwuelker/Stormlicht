321a73f6a8046e4cb6849181a144056bff5566e2
Simon Wülker <simon.wuelker@arcor.de>
2023-01-12 18:33:38 +0100

    http: Add tests to ringbuffer

--- web/http/src/compress/ringbuffer.rs
+++ web/http/src/compress/ringbuffer.rs
@@ -27,15 +27,61 @@ impl<T> RingBuffer<T> {
     /// Get the nth previous element from the ringbuffer.
     ///
     /// Note that the index is 0-based, so `nth_last(0)` returns the element that was
-    /// last pushed.
+    /// last pushed. However, you may not retrieve more than `size` previous elements.
+    /// 
+    /// # Panics
+    /// This function panics if `index >=  buffer size`
     pub fn nth_last(&self, index: usize) -> &T {
-        let unwrapped_index = (index + 1) % self.size();
+        assert!(index < self.size());
+        &self.elements[(self.ptr + self.size() - index - 1) % self.size()]
+        // let unwrapped_index = (index + 1) % self.size();
 
-        if self.ptr < unwrapped_index {
-            // wrap back around
-            &self.elements[self.ptr + self.size() - unwrapped_index]
-        } else {
-            &self.elements[self.ptr - unwrapped_index]
-        }
+        // if self.ptr < unwrapped_index {
+        //     // wrap back around
+        //     &self.elements[self.ptr + self.size() - unwrapped_index]
+        // } else {
+        //     &self.elements[self.ptr - unwrapped_index]
+        // }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::RingBuffer;
+
+    #[test]
+    fn test_ringbuffer() {
+        let mut buffer = RingBuffer::new(vec![3, 2, 1]);
+
+        assert_eq!(*buffer.nth_last(0), 1);
+        assert_eq!(*buffer.nth_last(1), 2);
+        assert_eq!(*buffer.nth_last(2), 3);
+
+        buffer.push(4);
+        // Internal buffer should now look like this:
+        // [4, 2, 1]
+        //     ^_ self.ptr
+
+        assert_eq!(*buffer.nth_last(0), 4);
+        assert_eq!(*buffer.nth_last(1), 1);
+        assert_eq!(*buffer.nth_last(2), 2);
+
+        buffer.push(5);
+        // Internal buffer should now look like this:
+        // [5, 4, 1]
+        //        ^_ self.ptr
+
+        assert_eq!(*buffer.nth_last(0), 5);
+        assert_eq!(*buffer.nth_last(1), 4);
+        assert_eq!(*buffer.nth_last(2), 1);
+
+        buffer.push(6);
+        // Internal buffer should now look like this:
+        // [5, 4, 6]
+        //  ^_ self.ptr
+
+        assert_eq!(*buffer.nth_last(0), 6);
+        assert_eq!(*buffer.nth_last(1), 5);
+        assert_eq!(*buffer.nth_last(2), 3);
     }
 }
