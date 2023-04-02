b7e8168c0f78c854eb034059881a1e87ece419a2
= <simon.wuelker@arcor.de>
2023-04-02 23:50:43 +0200

    dns: remove anyhow + thiserror from dependencies
    These are really more for user-facing libraries. We don't care about fancy
    error messages in a library and being able to match on the error is
    much more imporant.

--- web/dns/src/lib.rs
+++ web/dns/src/lib.rs
@@ -10,12 +10,12 @@ pub use domain::Domain;
 
 use message::Consume;
 
-use std::net::{IpAddr, Ipv4Addr, UdpSocket};
+use std::{
+    io,
+    net::{IpAddr, Ipv4Addr, UdpSocket},
+};
 
 use crate::message::Message;
-
-use anyhow::{Context, Result};
-use thiserror::Error;
 
 const MAX_DATAGRAM_SIZE: usize = 1024;
 const UDP_SOCKET: &str = "0.0.0.0:20000";
@@ -25,19 +25,17 @@ const MAX_RESOLUTION_STEPS: usize = 5;
 /// See [this list of root servers](https://www.iana.org/domains/root/servers).
 const ROOT_SERVER: IpAddr = IpAddr::V4(Ipv4Addr::new(199, 7, 83, 42));
 
-#[derive(Debug, Error)]
+#[derive(Debug)]
 pub enum DNSError {
-    #[error("Invalid DNS response")]
     InvalidResponse,
-    #[error("Could not resolve {:?}", .0)]
     CouldNotResolve(Domain),
-    #[error("Invalid domain: {:?}", .0)]
     InvalidDomain(Domain),
-    #[error("Maximum dns resolution steps exceeded")]
     MaxResolutionStepsExceeded,
+    UnexpectedID,
+    IO(io::Error),
 }
 
-pub fn resolve(domain: &Domain) -> Result<IpAddr> {
+pub fn resolve(domain: &Domain) -> Result<IpAddr, DNSError> {
     let mut nameserver = ROOT_SERVER;
 
     // incrementally resolve segments
@@ -62,17 +60,15 @@ pub fn resolve(domain: &Domain) -> Result<IpAddr> {
         }
 
         // We did not make any progress
-        return Err(DNSError::CouldNotResolve(domain.clone()).into());
+        return Err(DNSError::CouldNotResolve(domain.clone()));
     }
-    Err(DNSError::MaxResolutionStepsExceeded.into())
+    Err(DNSError::MaxResolutionStepsExceeded)
 }
 
-fn try_resolve_from(nameserver: IpAddr, domain: &Domain) -> Result<Message> {
+fn try_resolve_from(nameserver: IpAddr, domain: &Domain) -> Result<Message, DNSError> {
     // Bind a UDP socket
-    let socket = UdpSocket::bind(UDP_SOCKET)?;
-    socket
-        .connect((nameserver, 53))
-        .with_context(|| format!("Connecting to nameserver {nameserver:?}"))?;
+    let socket = UdpSocket::bind(UDP_SOCKET).map_err(DNSError::IO)?;
+    socket.connect((nameserver, 53)).map_err(DNSError::IO)?;
 
     // Send a DNS query
     let message = Message::new(domain);
@@ -80,17 +76,17 @@ fn try_resolve_from(nameserver: IpAddr, domain: &Domain) -> Result<Message> {
 
     let mut bytes = vec![0; message.size()];
     message.write_to_buffer(&mut bytes);
-    socket.send(&bytes)?;
+    socket.send(&bytes).map_err(DNSError::IO)?;
 
     // Read the DNS response
     let mut response = [0; MAX_DATAGRAM_SIZE];
-    let response_length = socket.recv(&mut response)?;
+    let response_length = socket.recv(&mut response).map_err(DNSError::IO)?;
 
     let (parsed_message, _) =
         Message::read(&response[..response_length], 0).map_err(|_| DNSError::InvalidResponse)?;
 
     if parsed_message.header.id != expected_id {
-        return Err(DNSError::InvalidResponse.into());
+        return Err(DNSError::UnexpectedID);
     }
 
     Ok(parsed_message)
