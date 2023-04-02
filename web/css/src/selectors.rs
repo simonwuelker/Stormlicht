ed136b23f6527960c1eda523cc5b86f28449cdd6
= <simon.wuelker@arcor.de>
2023-04-02 13:38:29 +0200

    css: Don't parse legacy pseudo elements as pseudo class selectors

--- web/css/src/selectors.rs
+++ web/css/src/selectors.rs
@@ -652,7 +652,14 @@ impl<'a> CSSParse<'a> for PseudoClassSelector<'a> {
         }
 
         match parser.next_token() {
-            Some(Token::Ident(ident)) => Ok(PseudoClassSelector::Ident(ident)),
+            Some(Token::Ident(ident)) => {
+                // LEGACY:
+                // The <pseudo-class-selector> production excludes the <legacy-pseudo-element-selector> production. (That is, :before/etc must never be parsed as a pseudo-class)
+                match ident.as_ref() {
+                    "before" | "after" | "first-line" | "first-letter" => Err(ParseError),
+                    _ => Ok(PseudoClassSelector::Ident(ident)),
+                }
+            },
             Some(Token::Function(function_name)) => {
                 let content = AnyValue::parse(parser)?;
                 if matches!(parser.next_token(), Some(Token::ParenthesisClose)) {
@@ -991,6 +998,25 @@ mod tests {
                 function_name: "foo".into(),
                 content: AnyValue(vec![Token::Ident("bar".into())])
             })
+        );
+
+        // For legacy compatibility reasons, the pseudo class production *excludes* the legacy pseudo element
+        // production
+        assert_eq!(
+            PseudoClassSelector::parse_from_str(":before"),
+            Err(ParseError)
+        );
+        assert_eq!(
+            PseudoClassSelector::parse_from_str(":after"),
+            Err(ParseError)
+        );
+        assert_eq!(
+            PseudoClassSelector::parse_from_str(":first-line"),
+            Err(ParseError)
+        );
+        assert_eq!(
+            PseudoClassSelector::parse_from_str(":first-letter"),
+            Err(ParseError)
         );
     }
 
