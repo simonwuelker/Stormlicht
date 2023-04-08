913afbfeed514e4923b10f25e249c200b3dc3ce3
Wuelle <simon.wuelker@arcor.de>
2023-04-08 12:00:34 +0200

    image: remove thiserror + anyhow from dependencies

--- image/src/png/chunks/plte.rs
+++ image/src/png/chunks/plte.rs
@@ -1,30 +1,28 @@
 //! [PLTE](https://www.w3.org/TR/png/#11PLTE) chunk
 
-use anyhow::Result;
-use thiserror::Error;
-
-#[derive(Debug, Error)]
+#[derive(Clone, Copy, Debug)]
 pub enum PaletteError {
-    #[error("Palette size must be a multiple of 3, found {}", .0)]
-    InvalidPaletteSize(usize),
+    InvalidPaletteSize,
 }
-#[derive(Debug)]
+
+#[derive(Clone, Copy, Debug)]
 pub struct Color {
     pub red: u8,
     pub green: u8,
     pub blue: u8,
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct Palette {
     pub colors: Vec<Color>,
 }
 
 impl Palette {
-    pub fn new(bytes: &[u8]) -> Result<Self> {
+    pub fn new(bytes: &[u8]) -> Result<Self, PaletteError> {
         if bytes.len() % 3 != 0 {
-            return Err(PaletteError::InvalidPaletteSize(bytes.len()).into());
+            return Err(PaletteError::InvalidPaletteSize);
         }
+
         let n_colors = bytes.len() / 3;
 
         let mut colors = Vec::with_capacity(n_colors);
