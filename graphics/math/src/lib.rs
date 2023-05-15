0b084ec0234ea9e9a9d11c69a8dc9ab531d43351
Simon Wülker <simon.wuelker@arcor.de>
2023-05-15 15:23:04 +0200

    graphics-math: Move Angle type into its own file

--- graphics/math/src/lib.rs
+++ graphics/math/src/lib.rs
@@ -1,9 +1,11 @@
 //! Common utilities used in graphics-related libraries
 
+mod angle;
 mod rect;
 mod transform;
 mod vec2d;
 
+pub use angle::Angle;
 pub use rect::Rectangle;
 pub use transform::AffineTransform;
-pub use vec2d::{Angle, Vec2D};
+pub use vec2d::Vec2D;
--- graphics/math/src/vec2d.rs
+++ graphics/math/src/vec2d.rs
@@ -1,3 +1,4 @@
+use crate::Angle;
 use std::ops::{Add, Div, Mul, Sub};
 
 /// Generate a trait impl for an operation involving two [Vec2D]s, like [Add] or [Sub]
@@ -108,56 +109,6 @@ impl Vec2D<f32> {
             x: self.x.round() as usize,
             y: self.y.round() as usize,
         }
-    }
-}
-
-/// Zero cost wrapper type for an `f32`.
-///
-/// This type exists since coordinates are also `f32`'s.
-/// It should enforce type safety to prevent coordinates from accidentally being
-/// used as angles.
-#[derive(Clone, Copy, Debug, Default)]
-pub struct Angle(f32);
-
-impl Angle {
-    /// Angles with a difference below this value (in radians) are considered equal
-    const MAX_ERROR: f32 = 0.01;
-
-    #[inline]
-    #[must_use]
-    pub fn from_radians(radians: f32) -> Self {
-        Self(radians)
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn diff(&self, other: &Self) -> Self {
-        let mut difference_in_radians = (self.0 - other.0).abs();
-
-        if std::f32::consts::PI < difference_in_radians {
-            difference_in_radians = std::f32::consts::TAU - difference_in_radians;
-        }
-
-        Self(difference_in_radians)
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn sin(&self) -> f32 {
-        self.0.sin()
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn cos(&self) -> f32 {
-        self.0.cos()
-    }
-}
-
-impl PartialEq for Angle {
-    #[must_use]
-    fn eq(&self, other: &Self) -> bool {
-        self.diff(other).0 < Self::MAX_ERROR
     }
 }
 
