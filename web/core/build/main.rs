bd2599abcb812ce5f652c62bd4171add6544b356
Simon Wülker <simon.wuelker@arcor.de>
2023-06-27 10:29:58 +0200

    web/core: Build named reference table at comptime

--- web/core/build/main.rs
+++ web/core/build/main.rs
@@ -2,6 +2,7 @@ mod inheritance;
 mod named_entities;
 
 pub fn main() {
+    println!("cargo:rerun-if-changed=build");
     inheritance::generate().expect("Generating inheritance code failed");
     named_entities::generate().expect("Generating html named entities failed");
 }
