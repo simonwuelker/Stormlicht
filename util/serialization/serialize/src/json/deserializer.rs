7437b7e63dde2e139ddc9c2ad9d3b4001b5f7d06
Simon Wülker <simon.wuelker@arcor.de>
2023-06-27 10:25:10 +0200

    serialization: Allow unicode escape in json string

--- util/serialization/serialize/src/json/deserializer.rs
+++ util/serialization/serialize/src/json/deserializer.rs
@@ -57,8 +57,34 @@ impl<'a> JsonDeserializer<'a> {
         self.source.get(self.position).copied()
     }
 
+    fn next(&mut self) -> Option<u8> {
+        let c = self.source.get(self.position).copied();
+        self.position += 1;
+        c
+    }
+
     fn bump(&mut self) {
         self.position += 1;
+    }
+
+    fn consume_hex_escape(&mut self) -> Option<u32> {
+        // Expect 4 hex digits
+        let a = self.next()?;
+        let b = self.next()?;
+        let c = self.next()?;
+        let d = self.next()?;
+
+        let to_digit = |c| match c {
+            b'0'..=b'9' => Some(c - b'0'),
+            b'a'..=b'z' => Some(c - b'a' + 10),
+            b'A'..=b'Z' => Some(c - b'A' + 10),
+            _ => None,
+        };
+        let value = (to_digit(a)? as u32) << 12
+            | (to_digit(b)? as u32) << 8
+            | (to_digit(c)? as u32) << 4
+            | (to_digit(d)? as u32);
+        Some(value)
     }
 
     fn next_token(&mut self) -> Option<Token> {
@@ -94,12 +120,57 @@ impl<'a> JsonDeserializer<'a> {
                     self.bump();
                     while let Some(c) = self.peek() {
                         self.bump();
-                        if c == b'"' {
-                            // SAFETY: The internal byte buffer came from &str which is guaranteed to be utf-8
-                            let string = unsafe { String::from_utf8_unchecked(string_bytes) };
-                            return Some(Token::String(string));
-                        } else {
-                            string_bytes.push(c);
+                        match c {
+                            b'"' => {
+                                // SAFETY: The internal byte buffer came from &str which is guaranteed to be utf-8
+                                let string = unsafe { String::from_utf8_unchecked(string_bytes) };
+                                return Some(Token::String(string));
+                            },
+                            b'\\' => {
+                                match self.next()? {
+                                    escaped_char @ (b'\\' | b'"' | b'/') => {
+                                        string_bytes.push(escaped_char)
+                                    },
+                                    b'b' => string_bytes.push(b'\x08'),
+                                    b'f' => string_bytes.push(b'\x0c'),
+                                    b'n' => string_bytes.push(b'\n'),
+                                    b'r' => string_bytes.push(b'\r'),
+                                    b't' => string_bytes.push(b'\t'),
+                                    b'u' => {
+                                        let mut reference_value = self.consume_hex_escape()?;
+                                        if (0xD800..=0xDBFF).contains(&reference_value) {
+                                            // UTF-16 surrogate
+                                            if self.next() != Some(b'\\') {
+                                                return None;
+                                            }
+                                            if self.next() != Some(b'u') {
+                                                return None;
+                                            }
+                                            let second_reference = self.consume_hex_escape()?;
+
+                                            if !(0xDC00..=0xDFFF).contains(&second_reference) {
+                                                return None;
+                                            }
+
+                                            reference_value = ((reference_value - 0xD800) << 10
+                                                | (second_reference - 0xDC00))
+                                                + 0x1_0000;
+                                        }
+
+                                        let referenced_char = char::from_u32(reference_value)?;
+                                        string_bytes.extend_from_slice(
+                                            referenced_char.encode_utf8(&mut [0_u8; 4]).as_bytes(),
+                                        );
+                                    },
+                                    _ => {
+                                        // Invalid escape character
+                                        return None;
+                                    },
+                                }
+                            },
+                            other => {
+                                string_bytes.push(other);
+                            },
                         }
                     }
 
@@ -256,6 +327,15 @@ mod tests {
         assert_eq!(tokens.next_token(), Some(Token::Colon));
         assert_eq!(tokens.next_token(), Some(Token::True));
         assert_eq!(tokens.next_token(), Some(Token::CurlyBraceClose));
+        assert_eq!(tokens.next_token(), None);
+    }
+
+    #[test]
+    fn unicode() {
+        // <UNICODE_UMBRELLA> + newline
+        let json = "\"\\u2602\\n\"";
+        let mut tokens = JsonDeserializer::new(json);
+        assert_eq!(tokens.next_token(), Some(Token::String("☂\n".to_string())));
         assert_eq!(tokens.next_token(), None);
     }
 }
