22b08d9a4127e2a03549b54857b6ff33c5a82f16
Simon Wülker <simon.wuelker@arcor.de>
2023-08-27 17:29:55 +0200

    web/core: remove dead code

--- web/core/src/css/layout/mod.rs
+++ web/core/src/css/layout/mod.rs
@@ -1,11 +1,9 @@
 mod box_dimensions;
 pub mod flow;
 mod pixels;
-mod styled_element;
 
 pub use box_dimensions::BoxDimensions;
 pub use pixels::CSSPixels;
-// pub use styled_element::StyledElement;
 
 #[derive(Clone, Copy, Debug)]
 pub struct Sides<T> {
@@ -14,26 +12,6 @@ pub struct Sides<T> {
     pub bottom: T,
     pub left: T,
 }
-
-// /// Describes a box size and the margins on either side of the box.
-// ///
-// /// This struct is used in both vertical and horizontal contexts.
-// ///
-// /// ## Horizontal
-// /// In a horizontal context, `content` is the elements width,
-// /// `margin_before` is the margin to the left side of the element and
-// /// `margin_after` is the margin to the right side of the element.
-// ///
-// /// ## Vertical
-// /// In a vertical context, `content` is the elements height,
-// /// `margin_before` is the margin on top of the element and
-// /// `margin_after` is the margin below the element.
-// #[derive(Clone, Copy, Debug)]
-// pub struct SizeAndMargins {
-//     pub content: CSSPixels,
-//     pub margin_before: CSSPixels,
-//     pub margin_after: CSSPixels,
-// }
 
 #[derive(Clone, Copy, Debug)]
 pub struct UsedSizeAndMargins {
