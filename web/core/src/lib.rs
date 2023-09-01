be74a640eab4b09d05a195410734ae92dd163301
Simon Wülker <simon.wuelker@arcor.de>
2023-09-01 13:00:37 +0200

    web/core: Add font cache for managing fonts

--- web/core/src/lib.rs
+++ web/core/src/lib.rs
@@ -1,6 +1,12 @@
-#![feature(exclusive_range_pattern, associated_type_defaults, let_chains)]
+#![feature(
+    exclusive_range_pattern,
+    associated_type_defaults,
+    let_chains,
+    lazy_cell
+)]
 
 mod browsing_context;
+mod font_cache;
 mod tree_debug;
 
 pub mod css;
@@ -9,4 +15,5 @@ pub mod html;
 pub mod infra;
 
 pub use browsing_context::BrowsingContext;
+pub use font_cache::{FontCache, FONT_CACHE};
 pub use tree_debug::{TreeDebug, TreeFormatter};
