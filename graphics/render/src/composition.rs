671fae84da8a4511ab69d4770aeef15007167cf9
Simon Wülker <simon.wuelker@arcor.de>
2023-09-12 00:23:03 +0200

    graphics/render: Allow setting display DPI

--- graphics/render/src/composition.rs
+++ graphics/render/src/composition.rs
@@ -9,9 +9,19 @@ use crate::Layer;
 /// Manages all the different [Layers](Layer) that should be rendered.
 ///
 /// Generally, there should never be a need to create more than one [Composition].
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone)]
 pub struct Composition {
+    dpi: (f32, f32),
     layers: HashMap<u16, Layer>,
+}
+
+impl Default for Composition {
+    fn default() -> Self {
+        Self {
+            dpi: (1., 1.),
+            layers: HashMap::default(),
+        }
+    }
 }
 
 impl Composition {
@@ -20,11 +30,25 @@ impl Composition {
     /// If there is no layer at the current index, a default layer is created and
     /// returned.
     pub fn get_or_insert_layer(&mut self, at_index: u16) -> &mut Layer {
-        self.layers.entry(at_index).or_default()
+        self.layers.entry(at_index).or_insert_with(|| {
+            let mut new_layer = Layer::default();
+            new_layer.scale(self.dpi.0, self.dpi.1);
+            new_layer
+        })
     }
 
     pub fn layers(&self) -> Iter<'_, u16, Layer> {
         self.layers.iter()
+    }
+
+    #[inline]
+    pub fn set_dpi(&mut self, dpi: (f32, f32)) {
+        self.dpi = dpi;
+    }
+
+    #[inline]
+    pub fn clear(&mut self) {
+        self.layers.clear();
     }
 
     pub fn render_to(&mut self, bitmap: &mut Bitmap<u32>) {
