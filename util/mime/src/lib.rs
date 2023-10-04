127ff320fb317698a590b3b43c94515f8164c32f
Simon Wülker <simon.wuelker@arcor.de>
2023-10-04 11:11:07 +0200

    web/mime: Support "data:" urls

--- util/mime/src/lib.rs
+++ util/mime/src/lib.rs
@@ -3,6 +3,8 @@
 // TODO: This spec utilizes a few methods from <https://fetch.spec.whatwg.org>, replace the current implementations
 // once fetch is implemented.
 
+#![feature(ascii_char_variants, ascii_char)]
+
 mod mime_type;
 mod resource;
 mod sniff;
