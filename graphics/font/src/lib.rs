b82404392fe756b2485f5924f14a9d4650459983
Simon Wülker <simon.wuelker@arcor.de>
2023-10-21 19:39:39 +0200

    graphics/font: Implement system font store

--- graphics/font/src/lib.rs
+++ graphics/font/src/lib.rs
@@ -3,12 +3,15 @@
 //! In the future, this should also cover [Font Shaping](https://fonts.google.com/knowledge/glossary/shaping)
 //! but for now, we are only concerned with rasterization.
 
-#![feature(array_chunks, iter_map_windows)]
+#![feature(array_chunks, iter_map_windows, result_flattening, cfg_match)]
 
 pub mod path;
+pub mod sources;
 mod stream;
 pub mod ttf;
 pub mod ttf_tables;
+
+pub use sources::SystemStore;
 
 pub use stream::{Readable, Stream};
 pub use ttf::Font;
