e5545a19ccc0b073731f7845ab652754809427c0
Alaska <simon.wuelker@arcor.de>
2023-11-10 19:52:14 +0100

    web/core: Generate some code at comptime using jinja2 templates
    Jinja2 is a lot more manageable and scales better than the previous
    approach, which was to simply generate code as a string inside
    build.rs

--- crates/web/core/build/main.rs
+++ crates/web/core/build/main.rs
@@ -1,8 +1,29 @@
+#![feature(exit_status_error)]
+
 mod inheritance;
-mod named_entities;
+
+use buildutils::PYTHON;
+use std::{env, path::PathBuf, process::Command};
 
 pub fn main() {
-    println!("cargo:rerun-if-changed=build");
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let download_dir = env::var_os("DOWNLOAD_DIR").unwrap();
+
+    println!("cargo:rerun-if-changed=templates");
+    println!("cargo:rerun-if-changed=build.py");
+    println!(
+        "cargo:rerun-if-changed={}",
+        PathBuf::from(&download_dir)
+            .join("html_named_entities.json")
+            .display()
+    );
+
+    Command::new(PYTHON.as_str())
+        .args(&["build.py".into(), out_dir, download_dir])
+        .status()
+        .expect("Failed to start python build script")
+        .exit_ok()
+        .expect("Failed to run python build script");
+
     inheritance::generate().expect("Generating inheritance code failed");
-    named_entities::generate().expect("Generating html named entities failed");
 }
