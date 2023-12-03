a5f76acdf5368274aa20ab28bd8675082d08c4f7
Wuelle <simon.wuelker@arcor.de>
2023-12-03 03:12:23 +0100

    graphics/font: Fix non-linux build

--- crates/graphics/font/src/sources/mod.rs
+++ crates/graphics/font/src/sources/mod.rs
@@ -14,6 +14,10 @@ cfg_match! {
         pub type SystemSource = fontconfig::FontConfig;
     }
     _ => {
+        use crate::Weight;
+
+        use std::path::PathBuf;
+
         pub struct Dummy;
 
         const FALLBACK_PATH: &str = concat!(
@@ -22,10 +26,13 @@ cfg_match! {
                 );
 
         impl FontStore for Dummy {
+            const NAME: &'static str = "Dummy";
+
             fn enumerate_system_fonts() -> Vec<SystemFont> {
                 let fallback_font = SystemFont {
                     path: PathBuf::from(FALLBACK_PATH),
-                    name: "Roboto-Medium".to_string()
+                    name: "Roboto-Medium".to_string(),
+                    weight_range: (Weight::NORMAL, Weight::NORMAL),
                 };
 
                 vec![
