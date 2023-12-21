96eb4a543faa07c0308bcf2ccc9294441f9783cc
Alaska <simon.wuelker@arcor.de>
2023-12-21 14:53:42 +0100

    crypto/hash: Create hash traits as abstractions over hash algorithms
    The std::hash::Hash trait is unsuited for this, as it doesn't support
    generic input/output block sizes.

--- crates/crypto/hash/src/lib.rs
+++ crates/crypto/hash/src/lib.rs
@@ -9,3 +9,29 @@ pub use adler32::{adler32, Adler32Hasher};
 pub use md5::{md5, Md5Hasher};
 pub use sha::{sha224, sha256, Sha224Hasher, Sha256Hasher};
 pub use {crc32::crc32, crc32::Crc32Hasher};
+
+/// Something that is able to calculate a checksum over arbitrary bytes.
+///
+/// Implementors of this trait do not provide any kind of security guarantees.
+/// For a cryptographically secure equivalent, use the [CryptographicHashAlgorithm] trait instead.
+pub trait HashAlgorithm: Default {
+    const BLOCK_SIZE_IN: usize;
+    const BLOCK_SIZE_OUT: usize;
+
+    fn update(&mut self, data: &[u8]);
+    fn finish(self) -> [u8; Self::BLOCK_SIZE_OUT];
+
+    fn hash(data: &[u8]) -> [u8; Self::BLOCK_SIZE_OUT] {
+        let mut hasher = Self::default();
+        hasher.update(data);
+        hasher.finish()
+    }
+}
+
+/// An algorithm suitable as a cryptographic hash function
+///
+/// Specifically, this implies that that the function
+/// cannot trivially be inverted.
+///
+/// This is a marker trait, no functionality beyond [HashAlgorithm] is required.
+pub trait CryptographicHashAlgorithm: HashAlgorithm {}
