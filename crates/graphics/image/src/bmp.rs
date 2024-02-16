e958674a995e6121e399cfabbf51017bb54b688e
Simon Wülker <simon.wuelker@arcor.de>
2024-02-16 02:04:33 +0100

    graphics/image: implement top-down BMPs

--- crates/graphics/image/src/bmp.rs
+++ crates/graphics/image/src/bmp.rs
@@ -19,6 +19,7 @@ pub enum Error {
     UnknownCompression,
     NonPalletizedCompression,
     PaletteTooSmall,
+    NegativeWidth,
 
     /// This image contains extreme values and cannot be parsed
     ///
@@ -40,6 +41,183 @@ enum Compression {
     None,
     Rle8Bit,
     Rle4Bit,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+enum Direction {
+    #[default]
+    BottomUp,
+    TopDown,
+}
+
+#[derive(Debug)]
+struct InfoHeader {
+    width: u32,
+    height: u32,
+    bits_per_pixel: BmpColorFormat,
+    compressed_image_size: u32,
+    direction: Direction,
+}
+
+impl InfoHeader {
+    #[must_use]
+    fn palette_size(&self) -> usize {
+        match self.bits_per_pixel {
+            BmpColorFormat::Monochrome => 2,
+            BmpColorFormat::Palletized4Bit => 16,
+            BmpColorFormat::Palletized8Bit => 256,
+            _ => 0,
+        }
+    }
+
+    #[must_use]
+    fn scanline_width(&self) -> usize {
+        let bytes_per_scanline = match self.bits_per_pixel {
+            BmpColorFormat::Monochrome => {
+                // 1 bit per pixel
+                align_up::<8>(self.width as usize) / 8
+            },
+            BmpColorFormat::Rgb16 => {
+                // 16 bits per pixel
+                2 * self.width as usize
+            },
+            BmpColorFormat::Rgb24 => {
+                // 24 bits per pixel
+                3 * self.width as usize
+            },
+            _ => todo!(),
+        };
+
+        align_up::<4>(bytes_per_scanline)
+    }
+
+    fn for_each_scanline<F>(&self, scanline_data: &[u8], mut f: F) -> Result<(), Error>
+    where
+        F: FnMut(&[u8]) -> Result<(), Error>,
+    {
+        let scanlines = scanline_data.chunks_exact(self.scanline_width());
+        if !scanlines.remainder().is_empty() {
+            log::warn!("Trailing bytes after last scanline");
+        }
+
+        if self.direction == Direction::TopDown {
+            for scanline in scanlines {
+                f(scanline)?;
+            }
+        } else {
+            for scanline in scanlines.rev() {
+                f(scanline)?;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn read(byte_stream: &mut ByteStream<'_>) -> Result<Self, Error> {
+        let info_header_size = byte_stream
+            .next_le_u32()
+            .ok_or(Error::UnexpectedEndOfFile)?;
+
+        if info_header_size != 40 {
+            log::warn!("Incorrect bmp info header size: Should be 40, is {info_header_size:?}");
+        }
+
+        let width = byte_stream
+            .next_le_i32()
+            .ok_or(Error::UnexpectedEndOfFile)?;
+
+        let width = u32::try_from(width).map_err(|_| Error::NegativeWidth)?;
+
+        let height = byte_stream
+            .next_le_i32()
+            .ok_or(Error::UnexpectedEndOfFile)?;
+
+        // A negative height indicates a top-down image
+        let (direction, height) = match u32::try_from(height) {
+            Ok(height) => (Direction::BottomUp, height),
+            Err(_) => (Direction::TopDown, height.abs() as u32),
+        };
+
+        let planes = byte_stream
+            .next_le_u16()
+            .ok_or(Error::UnexpectedEndOfFile)?;
+
+        if planes != 1 {
+            log::warn!("Unexpected number of planes, expected 1, got {planes:?}");
+        }
+
+        let bits_per_pixel = byte_stream
+            .next_le_u16()
+            .ok_or(Error::UnexpectedEndOfFile)?;
+
+        let colorformat = match bits_per_pixel {
+            1 => BmpColorFormat::Monochrome,
+            4 => BmpColorFormat::Palletized4Bit,
+            8 => BmpColorFormat::Palletized8Bit,
+            16 => BmpColorFormat::Rgb16,
+            24 => BmpColorFormat::Rgb24,
+            other => {
+                log::error!("No format known for the given number of bits per pixel: {other:?}");
+                return Err(Error::UnknownColorFormat);
+            },
+        };
+
+        let compression = byte_stream
+            .next_le_u32()
+            .ok_or(Error::UnexpectedEndOfFile)?;
+
+        let compression = match compression {
+            0 => Compression::None,
+            1 => Compression::Rle8Bit,
+            2 => Compression::Rle4Bit,
+            other => {
+                log::error!("Unknown compression format: {other:?}");
+                return Err(Error::UnknownCompression);
+            },
+        };
+
+        if compression != Compression::None
+            && matches!(colorformat, BmpColorFormat::Rgb16 | BmpColorFormat::Rgb24)
+        {
+            log::error!("Non-palletized images cannot be compressed (got compression {compression:?} and color format {colorformat:?}");
+            return Err(Error::NonPalletizedCompression);
+        }
+
+        if width > MAX_ACCEPTABLE_SIZE || height > MAX_ACCEPTABLE_SIZE {
+            log::error!("Refusing to allocate image of size {width}x{height}");
+            return Err(Error::RefuseToParse);
+        }
+
+        let compressed_image_size = byte_stream
+            .next_le_u32()
+            .ok_or(Error::UnexpectedEndOfFile)?;
+
+        let _x_pixels_per_meter = byte_stream
+            .next_le_u32()
+            .ok_or(Error::UnexpectedEndOfFile)?;
+
+        let _y_pixels_per_meter = byte_stream
+            .next_le_u32()
+            .ok_or(Error::UnexpectedEndOfFile)?;
+
+        let _colors_used = byte_stream
+            .next_le_u32()
+            .ok_or(Error::UnexpectedEndOfFile)?;
+
+        let _important_colors = byte_stream
+            .next_le_u32()
+            .ok_or(Error::UnexpectedEndOfFile)?;
+
+        let info_header = Self {
+            width,
+            height,
+            bits_per_pixel: colorformat,
+            compressed_image_size,
+            direction,
+        };
+
+        Ok(info_header)
+    }
 }
 
 pub fn decode(bytes: &[u8]) -> Result<DynamicTexture, Error> {
@@ -79,131 +257,13 @@ pub fn decode(bytes: &[u8]) -> Result<DynamicTexture, Error> {
         .ok_or(Error::UnexpectedEndOfFile)?;
 
     // Start of the Info Header
-    let info_header_size = byte_stream
-        .next_le_u32()
-        .ok_or(Error::UnexpectedEndOfFile)?;
-
-    if info_header_size != 40 {
-        log::warn!("Incorrect bmp info header size: Should be 40, is {info_header_size:?}");
-    }
-
-    let width = byte_stream
-        .next_le_u32()
-        .ok_or(Error::UnexpectedEndOfFile)?;
-
-    let height = byte_stream
-        .next_le_u32()
-        .ok_or(Error::UnexpectedEndOfFile)?;
-
-    let planes = byte_stream
-        .next_le_u16()
-        .ok_or(Error::UnexpectedEndOfFile)?;
-
-    if planes != 1 {
-        log::warn!("Unexpected number of planes, expected 1, got {planes:?}");
-    }
-
-    let bits_per_pixel = byte_stream
-        .next_le_u16()
-        .ok_or(Error::UnexpectedEndOfFile)?;
-
-    let (colorformat, palette_size) = match bits_per_pixel {
-        1 => (BmpColorFormat::Monochrome, 1),
-        4 => (BmpColorFormat::Palletized4Bit, 16),
-        8 => (BmpColorFormat::Palletized8Bit, 256),
-        16 => (BmpColorFormat::Rgb16, 0),
-        24 => (BmpColorFormat::Rgb24, 0),
-        other => {
-            log::error!("No format known for the given number of bits per pixel: {other:?}");
-            return Err(Error::UnknownColorFormat);
-        },
-    };
-
-    let compression = byte_stream
-        .next_le_u32()
-        .ok_or(Error::UnexpectedEndOfFile)?;
-
-    let compression = match compression {
-        0 => Compression::None,
-        1 => Compression::Rle8Bit,
-        2 => Compression::Rle4Bit,
-        other => {
-            log::error!("Unknown compression format: {other:?}");
-            return Err(Error::UnknownCompression);
-        },
-    };
-
-    if compression != Compression::None
-        && matches!(colorformat, BmpColorFormat::Rgb16 | BmpColorFormat::Rgb24)
-    {
-        log::error!("Non-palletized images cannot be compressed (got compression {compression:?} and color format {colorformat:?}");
-        return Err(Error::NonPalletizedCompression);
-    }
-
-    if width > MAX_ACCEPTABLE_SIZE || height > MAX_ACCEPTABLE_SIZE {
-        log::error!("Refusing to allocate image of size {width}x{height}");
-        return Err(Error::RefuseToParse);
-    }
-
-    let compressed_image_size = byte_stream
-        .next_le_u32()
-        .ok_or(Error::UnexpectedEndOfFile)?;
-
-    let x_pixels_per_meter = byte_stream
-        .next_le_u32()
-        .ok_or(Error::UnexpectedEndOfFile)?;
-
-    let y_pixels_per_meter = byte_stream
-        .next_le_u32()
-        .ok_or(Error::UnexpectedEndOfFile)?;
-
-    let colors_used = byte_stream
-        .next_le_u32()
-        .ok_or(Error::UnexpectedEndOfFile)?;
-
-    let important_colors = byte_stream
-        .next_le_u32()
-        .ok_or(Error::UnexpectedEndOfFile)?;
-
-    // Log all the info header information nicely
-    #[derive(Debug)]
-    #[allow(dead_code)]
-    struct InfoHeader {
-        size: u32,
-        width: u32,
-        height: u32,
-        planes: u16,
-        bits_per_pixel: BmpColorFormat,
-        compression: Compression,
-        compressed_image_size: u32,
-        x_pixels_per_meter: u32,
-        y_pixels_per_meter: u32,
-        colors_used: u32,
-        important_colors: u32,
-    }
-    let info_header = InfoHeader {
-        size: info_header_size,
-        width,
-        height,
-        planes,
-        bits_per_pixel: colorformat,
-        compression,
-        compressed_image_size,
-        x_pixels_per_meter,
-        y_pixels_per_meter,
-        colors_used,
-        important_colors,
-    };
+    let info_header = InfoHeader::read(&mut byte_stream)?;
 
     // Read the palette, if any
-    let used_palette_size = if colorformat == BmpColorFormat::Monochrome {
-        2
-    } else {
-        palette_size
-    };
+    let palette_size = info_header.palette_size();
 
-    let mut palette = Vec::with_capacity(used_palette_size);
-    for _ in 0..used_palette_size {
+    let mut palette = Vec::with_capacity(palette_size);
+    for _ in 0..palette_size {
         let [red, green, blue, reserved] =
             byte_stream.next_chunk().ok_or(Error::UnexpectedEndOfFile)?;
 
@@ -224,28 +284,24 @@ pub fn decode(bytes: &[u8]) -> Result<DynamicTexture, Error> {
 
     let image_data = byte_stream.remaining();
 
-    if image_data.len() != compressed_image_size as usize {
+    if image_data.len() != info_header.compressed_image_size as usize {
         log::warn!(
-            "Expected 0x{compressed_image_size:x} bytes of image data, found 0x{:x}",
+            "Expected 0x{:x} bytes of image data, found 0x{:x}",
+            info_header.compressed_image_size,
             image_data.len()
         );
     }
 
-    let texture = match colorformat {
+    let texture = match info_header.bits_per_pixel {
         BmpColorFormat::Rgb16 => {
             todo!("implement .bmp rgb16 format")
         },
         BmpColorFormat::Rgb24 => {
-            let scanline_width = align_up::<4>(3 * width as usize);
+            let mut texture_data =
+                Vec::with_capacity(info_header.width as usize * info_header.height as usize * 3);
 
-            let scanlines = image_data.chunks_exact(scanline_width);
-            if !scanlines.remainder().is_empty() {
-                log::warn!("Trailing bytes after last scanline");
-            }
-
-            let mut texture_data = Vec::with_capacity(width as usize * height as usize * 3);
-            for scanline in scanlines.rev() {
-                for pixel in scanline.chunks_exact(3).take(width as usize) {
+            info_header.for_each_scanline(image_data, |scanline| {
+                for pixel in scanline.chunks_exact(3).take(info_header.width as usize) {
                     let blue = pixel[0];
                     let green = pixel[1];
                     let red = pixel[2];
@@ -254,23 +310,23 @@ pub fn decode(bytes: &[u8]) -> Result<DynamicTexture, Error> {
                     texture_data.push(green);
                     texture_data.push(blue);
                 }
-            }
 
-            Texture::<Rgb<u8>, Vec<u8>>::from_data(texture_data, width as usize, height as usize)
-                .into()
+                Ok(())
+            })?;
+
+            Texture::<Rgb<u8>, Vec<u8>>::from_data(
+                texture_data,
+                info_header.width as usize,
+                info_header.height as usize,
+            )
+            .into()
         },
         BmpColorFormat::Monochrome => {
-            let bytes_per_scanline = align_up::<8>(width as usize) / 8;
-            let scanline_width = align_up::<4>(bytes_per_scanline);
+            let mut texture_data =
+                Vec::with_capacity(info_header.width as usize * info_header.height as usize * 3);
 
-            let scanlines = image_data.chunks_exact(scanline_width);
-            if !scanlines.remainder().is_empty() {
-                log::warn!("Trailing bytes after last scanline");
-            }
-
-            let mut texture_data = Vec::with_capacity(width as usize * height as usize * 3);
-            for scanline in scanlines.rev() {
-                for i in 0..width as usize {
+            info_header.for_each_scanline(image_data, |scanline| {
+                for i in 0..info_header.width as usize {
                     let byte_index = i / 8;
                     let bit_index = i % 8;
                     let palette_index = (scanline[byte_index] >> bit_index) & 1;
@@ -281,10 +337,16 @@ pub fn decode(bytes: &[u8]) -> Result<DynamicTexture, Error> {
 
                     texture_data.extend(pixel.channels());
                 }
-            }
 
-            Texture::<Rgb<u8>, Vec<u8>>::from_data(texture_data, width as usize, height as usize)
-                .into()
+                Ok(())
+            })?;
+
+            Texture::<Rgb<u8>, Vec<u8>>::from_data(
+                texture_data,
+                info_header.width as usize,
+                info_header.height as usize,
+            )
+            .into()
         },
         _ => todo!("implement palletized bmp images"),
     };
