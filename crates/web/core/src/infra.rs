b2326e8def8f4f4a049fdea15443a51a3a0f63ff
Simon Wülker <simon.wuelker@arcor.de>
2024-03-01 19:43:32 +0100

    web/core: Add "in parallel" algorithm
    For now this just spawns a thread, potentially
    with a name. Might want to do something fancier
    later.

--- crates/web/core/src/infra.rs
+++ crates/web/core/src/infra.rs
@@ -1,5 +1,7 @@
 //! <https://infra.spec.whatwg.org>
 
+use std::thread;
+
 /// <https://infra.spec.whatwg.org/#namespaces>
 #[derive(Clone, Copy, Debug, PartialEq, Default)]
 pub enum Namespace {
@@ -26,4 +28,19 @@ pub enum Namespace {
 /// <https://infra.spec.whatwg.org/#normalize-newlines>
 pub fn normalize_newlines(source: &str) -> String {
     source.replace("\r\n", "\n").replace('\r', "\n")
+}
+
+/// <https://html.spec.whatwg.org/multipage/infrastructure.html#in-parallel>
+pub fn in_parallel<F, T>(f: F, task_name: Option<String>) -> thread::JoinHandle<T>
+where
+    T: Send + 'static,
+    F: FnOnce() -> T + Send + 'static,
+{
+    let mut builder = thread::Builder::new();
+
+    if let Some(name) = task_name {
+        builder = builder.name(name)
+    }
+
+    builder.spawn(f).expect("Failed to spawn parallel task")
 }
