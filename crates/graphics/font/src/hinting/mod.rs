d04b0dc290513b79d883bd0e4714356ac8ca9c21
Simon Wülker <simon.wuelker@arcor.de>
2024-03-31 21:52:15 +0200

    fonts: Remove fixed point implementation and use the one in sl-std instead

--- crates/graphics/font/src/hinting/mod.rs
+++ crates/graphics/font/src/hinting/mod.rs
@@ -1,10 +1,9 @@
-mod fixed;
 mod graphics_state;
 mod interpreter;
 mod op;
 
-pub use fixed::Fixed;
 pub use graphics_state::GraphicsState;
 pub use interpreter::Interpreter;
+use sl_std::fixed::Fixed;
 
 pub type F26Dot6 = Fixed<6>;
