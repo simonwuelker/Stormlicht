64b71488b16bffe1cc2fb388ce24d1120179ad27
Simon Wülker <simon.wuelker@arcor.de>
2024-04-01 19:31:20 +0200

    web/core: Implement "normalize svg attributes for token" algorithm

--- crates/web/core/src/html/tokenization/token.rs
+++ crates/web/core/src/html/tokenization/token.rs
@@ -283,4 +283,73 @@ impl TagData {
     pub fn attributes(&self) -> &[(InternedString, InternedString)] {
         &self.attributes
     }
+
+    /// <https://html.spec.whatwg.org/multipage/parsing.html#adjust-svg-attributes>
+    pub fn adjust_svg_attributes(&mut self) {
+        for (key, _) in self.attributes.iter_mut() {
+            let adjusted_key = match key {
+                static_interned!("attributename") => static_interned!("attributeName"),
+                static_interned!("attributetype") => static_interned!("attributeType"),
+                static_interned!("basefrequency") => static_interned!("baseFrequency"),
+                static_interned!("baseprofile") => static_interned!("baseProfile"),
+                static_interned!("calcmode") => static_interned!("calcMode"),
+                static_interned!("clippathunits") => static_interned!("clipPathUnits"),
+                static_interned!("diffuseconstant") => static_interned!("diffuseConstant"),
+                static_interned!("edgemode") => static_interned!("edgeMode"),
+                static_interned!("filterunits") => static_interned!("filterUnits"),
+                static_interned!("glyphref") => static_interned!("glyphRef"),
+                static_interned!("gradienttransform") => static_interned!("gradientTransform"),
+                static_interned!("gradientunits") => static_interned!("gradientUnits"),
+                static_interned!("kernelmatrix") => static_interned!("kernelMatrix"),
+                static_interned!("kernelunitlength") => static_interned!("kernelUnitLength"),
+                static_interned!("keypoints") => static_interned!("keyPoints"),
+                static_interned!("keysplines") => static_interned!("keySplines"),
+                static_interned!("keytimes") => static_interned!("keyTimes"),
+                static_interned!("lengthadjust") => static_interned!("lengthAdjust"),
+                static_interned!("limitingconeangle") => static_interned!("limitingConeAngle"),
+                static_interned!("markerheight") => static_interned!("markerHeight"),
+                static_interned!("markerunits") => static_interned!("markerUnits"),
+                static_interned!("markerwidth") => static_interned!("markerWidth"),
+                static_interned!("maskcontentunits") => static_interned!("maskContentUnits"),
+                static_interned!("maskunits") => static_interned!("maskUnits"),
+                static_interned!("numoctaves") => static_interned!("numOctaves"),
+                static_interned!("pathlength") => static_interned!("pathLength"),
+                static_interned!("patterncontentunits") => static_interned!("patternContentUnits"),
+                static_interned!("patterntransform") => static_interned!("patternTransform"),
+                static_interned!("patternunits") => static_interned!("patternUnits"),
+                static_interned!("pointsatx") => static_interned!("pointsAtX"),
+                static_interned!("pointsaty") => static_interned!("pointsAtY"),
+                static_interned!("pointsatz") => static_interned!("pointsAtZ"),
+                static_interned!("preservealpha") => static_interned!("preserveAlpha"),
+                static_interned!("preserveaspectratio") => static_interned!("preserveAspectRatio"),
+                static_interned!("primitiveunits") => static_interned!("primitiveUnits"),
+                static_interned!("refx") => static_interned!("refX"),
+                static_interned!("refy") => static_interned!("refY"),
+                static_interned!("repeatcount") => static_interned!("repeatCount"),
+                static_interned!("repeatdur") => static_interned!("repeatDur"),
+                static_interned!("requiredextensions") => static_interned!("requiredExtensions"),
+                static_interned!("requiredfeatures") => static_interned!("requiredFeatures"),
+                static_interned!("specularconstant") => static_interned!("specularConstant"),
+                static_interned!("specularexponent") => static_interned!("specularExponent"),
+                static_interned!("spreadmethod") => static_interned!("spreadMethod"),
+                static_interned!("startoffset") => static_interned!("startOffset"),
+                static_interned!("stddeviation") => static_interned!("stdDeviation"),
+                static_interned!("stitchtiles") => static_interned!("stitchTiles"),
+                static_interned!("surfacescale") => static_interned!("surfaceScale"),
+                static_interned!("systemlanguage") => static_interned!("systemLanguage"),
+                static_interned!("tablevalues") => static_interned!("tableValues"),
+                static_interned!("targetx") => static_interned!("targetX"),
+                static_interned!("targety") => static_interned!("targetY"),
+                static_interned!("textlength") => static_interned!("textLength"),
+                static_interned!("viewbox") => static_interned!("viewBox"),
+                static_interned!("viewtarget") => static_interned!("viewTarget"),
+                static_interned!("xchannelselector") => static_interned!("xChannelSelector"),
+                static_interned!("ychannelselector") => static_interned!("yChannelSelector"),
+                static_interned!("zoomandpan") => static_interned!("zoomAndPan"),
+                _ => continue,
+            };
+
+            *key = adjusted_key;
+        }
+    }
 }
