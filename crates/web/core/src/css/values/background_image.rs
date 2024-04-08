2fcdead482bd87d27eaabb93aead6008e224f55a
Simon Wülker <simon.wuelker@arcor.de>
2024-04-08 23:26:40 +0200

    web/core: Parse CSS background-image property

---
+++ crates/web/core/src/css/values/background_image.rs
@@ -0,0 +1,97 @@
+use crate::{
+    css::{syntax::Token, CSSParse, ParseError, Parser},
+    static_interned,
+};
+
+use super::Url;
+
+#[derive(Clone, Debug)]
+pub struct BackgroundImage {
+    // TODO: The spec explicitly treats the "none" layers as layers (that are not rendered).
+    //       Is this necessary? Do we need to keep these around?
+    layers: Vec<Option<Url>>,
+}
+
+impl BackgroundImage {
+    #[must_use]
+    pub fn layers(&self) -> &[Option<Url>] {
+        &self.layers
+    }
+}
+
+impl<'a> CSSParse<'a> for BackgroundImage {
+    fn parse(parser: &mut Parser<'a>) -> Result<Self, ParseError> {
+        let first_layer = parse_single_layer(parser)?;
+        parser.skip_whitespace();
+
+        let mut layers = vec![first_layer];
+        while let Some(Token::Comma) = parser.peek_token() {
+            parser.next_token();
+            parser.skip_whitespace();
+            let layer = parse_single_layer(parser)?;
+            layers.push(layer);
+            parser.skip_whitespace();
+        }
+
+        Ok(Self { layers })
+    }
+}
+
+fn parse_single_layer(parser: &mut Parser<'_>) -> Result<Option<Url>, ParseError> {
+    let parsed_layer = match parser.peek_token() {
+        Some(Token::Ident(ident)) if ident == static_interned!("none") => {
+            parser.next_token();
+            None
+        },
+        _ => {
+            let url = Url::parse(parser)?;
+            Some(url)
+        },
+    };
+
+    Ok(parsed_layer)
+}
+
+impl Default for BackgroundImage {
+    fn default() -> Self {
+        Self { layers: vec![None] }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn fail_to_parse_background_image_with_no_layers() {
+        assert!(BackgroundImage::parse_from_str("").is_err());
+    }
+    #[test]
+    fn parse_background_image_url() {
+        let bg_image = BackgroundImage::parse_from_str("url(foobar)").unwrap();
+        let layers = bg_image.layers();
+
+        assert!(layers.len() == 1);
+        assert!(layers[0].is_some());
+    }
+
+    #[test]
+    fn parse_background_image_none() {
+        let bg_image = BackgroundImage::parse_from_str("none").unwrap();
+        let layers = bg_image.layers();
+
+        assert!(layers.len() == 1);
+        assert!(layers[0].is_none());
+    }
+
+    #[test]
+    fn parse_multiple_background_layers() {
+        let bg_image = BackgroundImage::parse_from_str("none, url(test), none").unwrap();
+        let layers = bg_image.layers();
+
+        assert!(layers.len() == 3);
+        assert!(layers[0].is_none());
+        assert!(layers[1].is_some());
+        assert!(layers[2].is_none());
+    }
+}
