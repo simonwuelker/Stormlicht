f3eef8d2a4490515f4a4b76859fc12e222daf31a
Simon Wülker <simon.wuelker@arcor.de>
2024-04-24 15:24:57 +0200

    serialize: Implement serializing enums

--- crates/util/serialization/serialize/src/json/serializer.rs
+++ crates/util/serialization/serialize/src/json/serializer.rs
@@ -1,7 +1,10 @@
 use std::{fmt, fmt::Write};
 
 use crate::{
-    serialization::{SerializeMap, SerializeSequence, SerializeStruct, Serializer},
+    serialization::{
+        SerializeMap, SerializeSequence, SerializeStruct, SerializeStructVariant,
+        SerializeTupleVariant, Serializer,
+    },
     Serialize,
 };
 
@@ -28,9 +31,16 @@ where
     W: Write,
 {
     type Error = fmt::Error;
+
     type SequenceSerializer<'a> = SequenceSerializer<'a, W> where Self: 'a, W: 'a;
+
     type MapSerializer<'a> = MapSerializer<'a, W> where Self: 'a;
+
     type StructSerializer<'a> = StructSerializer<'a, W> where Self: 'a;
+
+    type StructVariantSerializer<'a> = StructVariantSerializer<'a, W> where Self: 'a;
+
+    type TupleVariantSerializer<'a> = TupleVariantSerializer<'a, W> where Self: 'a;
 
     fn serialize_bool(&mut self, value: bool) -> Result<(), Self::Error> {
         match value {
@@ -51,32 +61,63 @@ where
 
     fn serialize_sequence<'a>(&'a mut self) -> Result<Self::SequenceSerializer<'a>, Self::Error> {
         write!(self.writer, "[")?;
-        let sequence_serializer = SequenceSerializer(self);
+        let sequence_serializer = SequenceSerializer(CommaSeparatedSequence::new(self));
 
         Ok(sequence_serializer)
     }
 
     fn serialize_map<'a>(&'a mut self) -> Result<Self::MapSerializer<'a>, Self::Error> {
         write!(self.writer, "{{")?;
-        let map_serializer = MapSerializer(self);
+        let map_serializer = MapSerializer(CommaSeparatedSequence::new(self));
 
         Ok(map_serializer)
     }
 
     fn serialize_struct<'a>(&'a mut self) -> Result<Self::StructSerializer<'a>, Self::Error> {
         write!(self.writer, "{{")?;
-        let struct_serializer = StructSerializer(MapSerializer(self));
+        let struct_serializer = StructSerializer(MapSerializer(CommaSeparatedSequence::new(self)));
 
         Ok(struct_serializer)
     }
+
+    fn serialize_struct_enum<'a>(
+        &'a mut self,
+        variant_name: &str,
+    ) -> Result<Self::StructVariantSerializer<'a>, Self::Error> {
+        write!(self.writer, "{{{variant_name:?}:")?;
+
+        let struct_variant_serializer = StructVariantSerializer(CommaSeparatedSequence::new(self));
+
+        Ok(struct_variant_serializer)
+    }
+
+    fn serialize_tuple_enum<'a>(
+        &'a mut self,
+        variant_name: &str,
+    ) -> Result<Self::TupleVariantSerializer<'a>, Self::Error> {
+        write!(self.writer, "{{{variant_name:?}:")?;
+
+        let struct_variant_serializer = TupleVariantSerializer(CommaSeparatedSequence::new(self));
+
+        Ok(struct_variant_serializer)
+    }
 }
 
-pub struct SequenceSerializer<'a, W>(&'a mut JsonSerializer<W>);
+struct CommaSeparatedSequence<'a, W> {
+    serializer: &'a mut JsonSerializer<W>,
+    is_first_element: bool,
+}
 
-pub struct MapSerializer<'a, W>(&'a mut JsonSerializer<W>);
+pub struct SequenceSerializer<'a, W>(CommaSeparatedSequence<'a, W>);
+
+pub struct MapSerializer<'a, W>(CommaSeparatedSequence<'a, W>);
 
 // Structs are just maps with string keys
 pub struct StructSerializer<'a, W>(MapSerializer<'a, W>);
+
+pub struct StructVariantSerializer<'a, W>(CommaSeparatedSequence<'a, W>);
+
+pub struct TupleVariantSerializer<'a, W>(CommaSeparatedSequence<'a, W>);
 
 impl<'a, W> SerializeSequence for SequenceSerializer<'a, W>
 where
@@ -86,15 +127,15 @@ where
 
     fn serialize_element<T>(&mut self, element: &T) -> Result<(), Self::Error>
     where
-        T: crate::serialization::Serialize,
+        T: Serialize,
     {
-        element.serialize_to(self.0)?;
-        write!(self.0.writer, ",")?;
+        self.0.start_element()?;
+        element.serialize_to(self.0.serializer)?;
         Ok(())
     }
 
-    fn finish(self) -> Result<(), Self::Error> {
-        write!(self.0.writer, "]")
+    fn finish(mut self) -> Result<(), Self::Error> {
+        write!(self.0.writer(), "]")
     }
 }
 
@@ -106,18 +147,19 @@ where
 
     fn serialize_key_value_pair<K, V>(&mut self, key: &K, value: &V) -> Result<(), Self::Error>
     where
-        K: crate::serialization::Serialize,
-        V: crate::serialization::Serialize,
+        K: Serialize,
+        V: Serialize,
     {
-        key.serialize_to(self.0)?;
-        write!(self.0.writer, ":")?;
-        value.serialize_to(self.0)?;
-        write!(self.0.writer, ",")?;
+        self.0.start_element()?;
+
+        key.serialize_to(self.0.serializer)?;
+        write!(self.0.writer(), ":")?;
+        value.serialize_to(self.0.serializer)?;
         Ok(())
     }
 
-    fn finish(self) -> Result<(), Self::Error> {
-        write!(self.0.writer, "}}")
+    fn finish(mut self) -> Result<(), Self::Error> {
+        write!(self.0.writer(), "}}")
     }
 }
 
@@ -129,12 +171,85 @@ where
 
     fn serialize_field<T>(&mut self, name: &str, value: &T) -> Result<(), Self::Error>
     where
-        T: crate::serialization::Serialize,
+        T: Serialize,
     {
         self.0.serialize_key_value_pair(&name, value)
     }
 
     fn finish(self) -> Result<(), Self::Error> {
         self.0.finish()
+    }
+}
+
+impl<'a, W> SerializeStructVariant for StructVariantSerializer<'a, W>
+where
+    W: Write,
+{
+    type Error = <JsonSerializer<W> as Serializer>::Error;
+
+    fn serialize_field<T>(&mut self, name: &str, value: &T) -> Result<(), Self::Error>
+    where
+        T: Serialize,
+    {
+        self.0.start_element()?;
+        name.serialize_to(self.0.serializer)?;
+        write!(self.0.serializer.writer, ":")?;
+        value.serialize_to(self.0.serializer)?;
+
+        Ok(())
+    }
+
+    fn finish(self) -> Result<(), Self::Error> {
+        write!(self.0.serializer.writer, "}}}}")
+    }
+}
+
+impl<'a, W> SerializeTupleVariant for TupleVariantSerializer<'a, W>
+where
+    W: Write,
+{
+    type Error = <JsonSerializer<W> as Serializer>::Error;
+
+    fn serialize_element<T>(&mut self, element: &T) -> Result<(), Self::Error>
+    where
+        T: Serialize,
+    {
+        self.0.start_element()?;
+        element.serialize_to(self.0.serializer)?;
+
+        Ok(())
+    }
+
+    fn finish(self) -> Result<(), Self::Error> {
+        write!(self.0.serializer.writer, "]}}")
+    }
+}
+
+impl<'a, W> CommaSeparatedSequence<'a, W>
+where
+    W: Write,
+{
+    #[must_use]
+    fn new(serializer: &'a mut JsonSerializer<W>) -> Self {
+        Self {
+            serializer,
+            is_first_element: true,
+        }
+    }
+
+    fn start_element(&mut self) -> Result<(), <JsonSerializer<W> as Serializer>::Error> {
+        if self.is_first_element {
+            // There is no comma before the first element
+            self.is_first_element = false;
+            Ok(())
+        } else {
+            write!(self.serializer.writer, ",")
+        }
+    }
+
+    /// Convenience function to access the underlying writer
+    #[must_use]
+    fn writer(&mut self) -> &mut W {
+        &mut self.serializer.writer
     }
 }
--- crates/util/serialization/serialize/src/serialization/mod.rs
+++ crates/util/serialization/serialize/src/serialization/mod.rs
@@ -21,6 +21,14 @@ pub trait Serializer {
     where
         Self: 'a;
 
+    type TupleVariantSerializer<'a>: SerializeTupleVariant<Error = Self::Error>
+    where
+        Self: 'a;
+
+    type StructVariantSerializer<'a>: SerializeStructVariant<Error = Self::Error>
+    where
+        Self: 'a;
+
     fn serialize_bool(&mut self, value: bool) -> Result<(), Self::Error>;
 
     fn serialize_string(&mut self, value: &str) -> Result<(), Self::Error>;
@@ -32,6 +40,23 @@ pub trait Serializer {
     fn serialize_map<'a>(&'a mut self) -> Result<Self::MapSerializer<'a>, Self::Error>;
 
     fn serialize_struct<'a>(&'a mut self) -> Result<Self::StructSerializer<'a>, Self::Error>;
+
+    /// Serialize an enum variant without any associated data.
+    ///
+    /// Implementations should not usually need to override this.
+    fn serialize_enum<'a, F>(&'a mut self, variant_name: &str) -> Result<(), Self::Error> {
+        self.serialize_tuple_enum(variant_name)?.finish()
+    }
+
+    fn serialize_tuple_enum<'a>(
+        &'a mut self,
+        variant_name: &str,
+    ) -> Result<Self::TupleVariantSerializer<'a>, Self::Error>;
+
+    fn serialize_struct_enum<'a>(
+        &'a mut self,
+        variant_name: &str,
+    ) -> Result<Self::StructVariantSerializer<'a>, Self::Error>;
 }
 
 pub trait SerializeSequence {
@@ -59,6 +84,26 @@ pub trait SerializeStruct {
     type Error;
 
     fn serialize_field<T>(&mut self, name: &str, value: &T) -> Result<(), Self::Error>
+    where
+        T: Serialize;
+
+    fn finish(self) -> Result<(), Self::Error>;
+}
+
+pub trait SerializeStructVariant {
+    type Error;
+
+    fn serialize_field<T>(&mut self, name: &str, value: &T) -> Result<(), Self::Error>
+    where
+        T: Serialize;
+
+    fn finish(self) -> Result<(), Self::Error>;
+}
+
+pub trait SerializeTupleVariant {
+    type Error;
+
+    fn serialize_element<T>(&mut self, element: &T) -> Result<(), Self::Error>
     where
         T: Serialize;
 
