95d00a2359d6fcc665261911ad39f9aa84cbd832
Simon Wülker <simon.wuelker@arcor.de>
2024-04-24 15:28:07 +0200

    serialize-derive: Use new struct serialization logic

--- crates/util/serialization/serialize-derive/src/serialize.rs
+++ crates/util/serialization/serialize-derive/src/serialize.rs
@@ -20,15 +20,13 @@ pub(crate) fn serialize_struct(input: syn::ItemStruct) -> TokenStream {
             fn serialize_to<T: ::serialize::Serializer>(&self, serializer: &mut T) -> Result<(), T::Error> {
                 use ::serialize::serialization::SerializeStruct;
 
-                serializer.serialize_struct(|struct_serializer| {
-                    #(
-                        struct_serializer.serialize_field(
-                            stringify!(#idents), &self.#idents,
-                        )?;
-                    )*
-
-                    Ok(())
-                })?;
+                let mut struct_serializer = serializer.serialize_struct()?;
+                #(
+                    struct_serializer.serialize_field(
+                        stringify!(#idents), &self.#idents,
+                    )?;
+                )*
+                struct_serializer.finish()?;
 
                 Ok(())
             }
