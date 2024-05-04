947dedd3bc65e0a70456dae0869d7116ee1eb07b
Simon Wülker <simon.wuelker@arcor.de>
2024-05-04 15:56:06 +0200

    serialize: Impl Serialize for IpAddr

--- crates/util/serialization/serialize/src/serialization/impls.rs
+++ crates/util/serialization/serialize/src/serialization/impls.rs
@@ -1,6 +1,6 @@
-use std::{ascii, collections::HashMap};
+use std::{ascii, collections::HashMap, net};
 
-use super::{Serialize, SerializeMap, SerializeSequence, Serializer};
+use super::{Serialize, SerializeMap, SerializeSequence, SerializeTupleVariant, Serializer};
 
 impl<'a> Serialize for &'a str {
     fn serialize_to<S>(&self, serializer: &mut S) -> Result<(), S::Error>
@@ -143,5 +143,57 @@ where
         S: Serializer,
     {
         serializer.serialize_option(self)
+    }
+}
+
+impl<T, const N: usize> Serialize for [T; N]
+where
+    T: Serialize,
+{
+    fn serialize_to<S>(&self, serializer: &mut S) -> Result<(), S::Error>
+    where
+        S: Serializer,
+    {
+        self.as_slice().serialize_to(serializer)
+    }
+}
+
+impl Serialize for net::IpAddr {
+    fn serialize_to<S>(&self, serializer: &mut S) -> Result<(), S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            Self::V4(ipv4) => {
+                let mut variant_serializer = serializer.serialize_tuple_enum("v4")?;
+                variant_serializer.serialize_element(ipv4)?;
+                variant_serializer.finish()?;
+            },
+            Self::V6(ipv6) => {
+                let mut variant_serializer = serializer.serialize_tuple_enum("v6")?;
+                variant_serializer.serialize_element(ipv6)?;
+                variant_serializer.finish()?;
+            },
+        }
+
+        Ok(())
+    }
+}
+
+impl Serialize for net::Ipv4Addr {
+    fn serialize_to<S>(&self, serializer: &mut S) -> Result<(), S::Error>
+    where
+        S: Serializer,
+    {
+        self.octets().serialize_to(serializer)
+    }
+}
+
+impl Serialize for net::Ipv6Addr {
+    fn serialize_to<S>(&self, serializer: &mut S) -> Result<(), S::Error>
+    where
+        S: Serializer,
+    {
+        self.octets().serialize_to(serializer)
     }
 }
