abb47878c3beb551630116dc99103f03cfee196c
Simon Wülker <simon.wuelker@arcor.de>
2024-05-04 15:23:04 +0200

    serialize-json: Add a few unit tests

--- crates/util/serialization/serialize-json/src/lib.rs
+++ crates/util/serialization/serialize-json/src/lib.rs
@@ -5,3 +5,34 @@ mod serializer;
 
 pub use deserializer::{JsonDeserializer, JsonError};
 pub use serializer::{JsonSerializer, MapSerializer, SequenceSerializer};
+
+#[cfg(test)]
+mod tests {
+    use serialize::{Deserialize, Serialize};
+
+    use super::*;
+
+    fn serialize_deserialize<T>(value: T) -> T
+    where
+        T: Serialize + Deserialize,
+    {
+        let serialized = JsonSerializer::serialize_to_string(value).unwrap();
+        let mut deserializer = JsonDeserializer::new(&serialized);
+        let deserialized = T::deserialize(&mut deserializer).unwrap();
+
+        deserialized
+    }
+
+    #[test]
+    fn serialize_deserialize_string() {
+        let s = "foo".to_string();
+        assert_eq!(serialize_deserialize(s.clone()), s);
+    }
+
+    #[test]
+    fn serialize_deserialize_option() {
+        let value: Option<usize> = Some(3);
+
+        assert_eq!(serialize_deserialize(value), value)
+    }
+}
