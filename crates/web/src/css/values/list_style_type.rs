76205f11db52e364294d7c5c53125231661d63c3
Simon Wülker <simon.wuelker@arcor.de>
2024-05-14 12:04:20 +0200

    web: Parse list-style-type property

---
+++ crates/web/src/css/values/list_style_type.rs
@@ -0,0 +1,71 @@
+//! <https://drafts.csswg.org/css-lists/#propdef-list-style-type>
+
+use crate::{
+    css::{syntax::Token, CSSParse, ParseError, Parser},
+    static_interned, InternedString,
+};
+
+/// <https://drafts.csswg.org/css-counter-styles-3/#typedef-counter-style-name>
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum CounterStyle {
+    /// <https://drafts.csswg.org/css-counter-styles-3/#decimal>
+    Decimal,
+
+    /// <https://drafts.csswg.org/css-counter-styles-3/#disc>
+    Disc,
+
+    /// <https://drafts.csswg.org/css-counter-styles-3/#square>
+    Square,
+
+    /// <https://drafts.csswg.org/css-counter-styles-3/#disclosure-open>
+    DisclosureOpen,
+
+    /// <https://drafts.csswg.org/css-counter-styles-3/#disclosure-closed>
+    DisclosureClosed,
+}
+
+/// <https://drafts.csswg.org/css-lists/#propdef-list-style-type>
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum ListStyleType {
+    CounterStyle(CounterStyle),
+    String(InternedString),
+    None,
+}
+
+impl CounterStyle {
+    #[must_use]
+    pub fn as_str(&self) -> InternedString {
+        match self {
+            Self::Decimal | // FIXME: implement decimal
+            Self::Disc => static_interned!("•"),
+            Self::Square => static_interned!("▪"),
+            Self::DisclosureOpen => static_interned!("▾"),
+            Self::DisclosureClosed => static_interned!("▸")
+        }
+    }
+}
+
+impl<'a> CSSParse<'a> for ListStyleType {
+    fn parse(parser: &mut Parser<'a>) -> Result<Self, ParseError> {
+        let list_style_type = match parser.next_token_ignoring_whitespace() {
+            Some(Token::Ident(static_interned!("none"))) => Self::None,
+            Some(Token::Ident(static_interned!("decimal"))) => {
+                Self::CounterStyle(CounterStyle::Decimal)
+            },
+            Some(Token::Ident(static_interned!("disc"))) => Self::CounterStyle(CounterStyle::Disc),
+            Some(Token::Ident(static_interned!("square"))) => {
+                Self::CounterStyle(CounterStyle::Square)
+            },
+            Some(Token::Ident(static_interned!("disclosure-open"))) => {
+                Self::CounterStyle(CounterStyle::DisclosureOpen)
+            },
+            Some(Token::Ident(static_interned!("disclosure-closed"))) => {
+                Self::CounterStyle(CounterStyle::DisclosureClosed)
+            },
+            Some(Token::String(s)) => Self::String(s),
+            _ => return Err(ParseError),
+        };
+
+        Ok(list_style_type)
+    }
+}
