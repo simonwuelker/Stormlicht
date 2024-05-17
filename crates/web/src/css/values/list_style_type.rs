c8c3e2c079c0d76da5c10b79006300f6b40f0245
Simon Wülker <simon.wuelker@arcor.de>
2024-05-17 11:45:56 +0200

    web: Add spacing after ::marker element
    This is pretty arbitrary, but looks better than
    no spacing at all /shrug

--- crates/web/src/css/values/list_style_type.rs
+++ crates/web/src/css/values/list_style_type.rs
@@ -35,12 +35,16 @@ pub enum ListStyleType {
 impl CounterStyle {
     #[must_use]
     pub fn as_str(&self) -> String {
+        // FIXME: I don't think the spacing between
+        //        the ::marker element and the list-item
+        //        is specified - valid to use NBSP (U+00A0) here?
         match self {
             Self::Decimal | // FIXME: implement decimal
-            Self::Disc => String::from("•"),
-            Self::Square => String::from("▪"),
-            Self::DisclosureOpen => String::from("▾"),
-            Self::DisclosureClosed => String::from("▸")
+            Self::Disc => String::from("•\u{00A0}"),
+            Self::Square => String::from("▪\u{00A0}"),
+            Self::DisclosureOpen => String::from("▾\u{00A0}"),
+            // FIXME: This should respect the writing type
+            Self::DisclosureClosed => String::from("▸\u{00A0}")
         }
     }
 }
