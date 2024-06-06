34a2a805afab71b9f624c1bb77e37d8e280776be
Simon Wülker <simon.wuelker@arcor.de>
2024-06-07 00:08:48 +0200

    web: Handle hovered nodes in browsing context
    This implements support for things like the
    :hover pseudoclass in CSS

--- crates/web/src/browsing_context.rs
+++ crates/web/src/browsing_context.rs
@@ -12,6 +12,7 @@ use crate::{
         StyleComputer, Stylesheet,
     },
     dom::{dom_objects, DomPtr},
+    event,
     html::{self, tokenization::IgnoreParseErrors},
 };
 
@@ -26,6 +27,8 @@ struct CurrentPage {
     document: DomPtr<dom_objects::Document>,
     fragment_tree: FragmentTree,
     stylesheets: Vec<Stylesheet>,
+    hovered_element: Option<DomPtr<dom_objects::Element>>,
+    needs_relayout: bool,
 }
 
 #[derive(Debug)]
@@ -68,6 +71,8 @@ impl BrowsingContext {
             document,
             fragment_tree: FragmentTree::default(),
             stylesheets,
+            hovered_element: None,
+            needs_relayout: true,
         };
 
         self.current_page = Some(current_page);
@@ -85,21 +90,9 @@ impl BrowsingContext {
             height: Pixels(viewport_size.1 as f32),
         };
 
-        let layout_start = time::Instant::now();
-        let style_computer = StyleComputer::new(&current_page.stylesheets);
-
-        // Build a box tree for the parsed document
-        let box_tree = BoxTree::new(current_page.document.clone(), style_computer, viewport_size);
-        log::info!("\n{:?}", box_tree);
-
-        // Build a fragment tree by fragmenting the boxes
-        current_page.fragment_tree = box_tree.compute_fragments(viewport_size);
-
-        let layout_end = time::Instant::now();
-        log::info!(
-            "Layout took {}ms",
-            layout_end.duration_since(layout_start).as_millis()
-        );
+        if current_page.needs_relayout {
+            current_page.layout(viewport_size);
+        }
 
         // Paint the fragment_tree to the screen
         let mut painter = Painter::default();
@@ -108,5 +101,73 @@ impl BrowsingContext {
             .fill_display_list(&mut painter, viewport_size);
 
         painter.paint(to);
+    }
+
+    pub fn handle_mouse_event(&mut self, mouse_event: event::MouseEvent) {
+        let Some(current_page) = &mut self.current_page else {
+            return;
+        };
+
+        let mouse_position = mouse_event.position.map(|x| Pixels(x as f32));
+
+        let hovered_element: Option<DomPtr<dom_objects::Element>> = current_page
+            .fragment_tree
+            .hit_test(mouse_position)
+            .and_then(|fragment| fragment.dom_node())
+            .and_then(|node| node.try_into_type());
+
+        current_page.update_hovered_element(hovered_element);
+    }
+}
+
+impl CurrentPage {
+    fn layout(&mut self, viewport_size: Size<Pixels>) {
+        let layout_start = time::Instant::now();
+        let style_computer = StyleComputer::new(&self.stylesheets);
+
+        // Build a box tree for the parsed document
+        let box_tree = BoxTree::new(self.document.clone(), style_computer, viewport_size);
+        log::info!("\n{:?}", box_tree);
+
+        // Build a fragment tree by fragmenting the boxes
+        self.fragment_tree = box_tree.compute_fragments(viewport_size);
+
+        let layout_end = time::Instant::now();
+        log::info!(
+            "Layout took {}ms",
+            layout_end.duration_since(layout_start).as_millis()
+        );
+
+        self.needs_relayout = false;
+    }
+
+    fn update_hovered_element(&mut self, hovered_element: Option<DomPtr<dom_objects::Element>>) {
+        // Update hover state and invalidate layout if necessary
+        match (hovered_element.clone(), self.hovered_element.clone()) {
+            (Some(new_element), Some(old_element)) => {
+                if new_element.ptr_eq(&old_element) {
+                    // Nothing to do
+                    return;
+                }
+                old_element.borrow_mut().set_hovered(false);
+                new_element.borrow_mut().set_hovered(true);
+            },
+            (Some(new_element), None) => {
+                new_element.borrow_mut().set_hovered(true);
+            },
+            (None, Some(old_element)) => {
+                old_element.borrow_mut().set_hovered(false);
+            },
+            (None, None) => return,
+        }
+        self.hovered_element = hovered_element;
+
+        // Changing the hovered element can change the CSS rules that apply (via the :hover pseudoclass)
+        // and therefore invalidates layout
+        self.invalidate_layout();
+    }
+
+    fn invalidate_layout(&mut self) {
+        self.needs_relayout = true;
     }
 }
