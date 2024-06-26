e32adf1b8f7d1dd188d4f7e94c4a5ffbbdcf3157
Simon Wülker <simon.wuelker@arcor.de>
2024-06-27 00:54:29 +0200

    web: Add concept of Document URL

--- crates/web/src/dom/dom_objects/document.rs
+++ crates/web/src/dom/dom_objects/document.rs
@@ -1,15 +1,28 @@
 use dom_derive::inherit;
+use url::URL;
 
 use super::Node;
 
 /// <https://dom.spec.whatwg.org/#interface-document>
 #[inherit(Node)]
 pub struct Document {
+    /// <https://dom.spec.whatwg.org/#concept-document-url>
+    url: URL,
+
     charset: String,
 }
 
 impl Document {
+    #[must_use]
     pub fn charset(&self) -> &str {
         &self.charset
+    }
+
+    pub fn url(&self) -> &URL {
+        &self.url
+    }
+
+    pub fn set_url(&mut self, url: URL) {
+        self.url = url;
     }
 }
