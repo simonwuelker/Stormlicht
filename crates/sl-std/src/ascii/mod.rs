d9612693ada6351778126768661603dd75414e3e
Simon Wülker <simon.wuelker@arcor.de>
2024-07-01 00:50:39 +0200

    sl-std: Add ascii::Char::is_alphabetic

--- crates/sl-std/src/ascii/mod.rs
+++ crates/sl-std/src/ascii/mod.rs
@@ -10,10 +10,14 @@ pub use write::Write;
 
 pub use std::ascii::Char;
 
+/// Extensions for [Char]
 pub trait AsciiCharExt {
     /// <https://infra.spec.whatwg.org/#ascii-whitespace>
     fn is_whitespace(&self) -> bool;
     fn is_newline(&self) -> bool;
+
+    /// Checks if the character is in the range `a-z` or `A-Z`, inclusive
+    fn is_alphabetic(&self) -> bool;
     fn to_lowercase(&self) -> Self;
 }
 
@@ -23,6 +27,11 @@ impl AsciiCharExt for Char {
             self,
             Self::LineTabulation | Self::LineFeed | Self::FormFeed | Self::Space
         )
+    }
+
+    fn is_alphabetic(&self) -> bool {
+        (Char::CapitalA..=Char::CapitalZ).contains(self)
+            || (Char::SmallA..=Char::SmallZ).contains(self)
     }
 
     fn is_newline(&self) -> bool {
